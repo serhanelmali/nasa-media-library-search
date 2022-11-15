39cc41025ed145fc1da0d6ac91c27f0620297ed6
Serhan Elmalı <serhanelmali@gmail.com>
2022-11-16 02:04:41 +0300

    feat: adds react-query client provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "App";
 import GlobalStyle from "styles/globalStyles";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
