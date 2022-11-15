e21ce49ce4a9dfbf8e7b37af54b8b5619c2f517c
Serhan Elmalı <serhanelmali@gmail.com>
2022-11-16 01:57:36 +0300

    feat: adds normalize.css and absolute path transforms

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import GlobalStyle from "./globalStyles";
+import App from "App";
+import GlobalStyle from "styles/globalStyles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
