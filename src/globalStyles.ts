6b3e4da358d30f45de15ab642ed412adfd4c09e9
Serhan Elmalı <serhanelmali@gmail.com>
2022-11-16 01:44:56 +0300

    feat: adds globalStyles

---
+++ src/globalStyles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body{
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Arial, Helvetica, sans-serif;
+}
+`;
+
+export default GlobalStyle;
