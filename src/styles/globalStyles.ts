d14b8cb0cc2dc2b2c574ca2ca7f1784b6fc19358
Serhan Elmalı <serhanelmali@gmail.com>
2022-11-23 14:31:44 +0300

    adds font and some global styling

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,10 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing: border-box;
+}
 body{
+    @import url('https://fonts.googleapis.com/css2?family=Exo+2:wght@200;300;500;700&family=IBM+Plex+Serif:wght@300;400;700&display=swap');
+    color: white;
+    font-family: 'Exo 2', sans-serif;
     margin: 0;
-    padding: 0;
-    font-family: Open-Sans, Arial, Helvetica, sans-serif;
+    padding: 0;box-sizing: border-box;
 }
 `;
 
